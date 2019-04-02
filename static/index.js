224e29acc363b6d7dee99bb1541fe0feb5a4aced
lfonzi62 <lfontenette@berkeley.edu>
2019-04-01 17:18:38 -0700

    fixed the issue of calling google maps api in layout.html

--- static/index.js
+++ static/index.js
@@ -2,3 +2,11 @@ function activePlacesSearch(){
     var input = document.getElementById('location');
     var autocomplete = new google.maps.places.Autocomplete(input);
     }
+
+    var map;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      });
+    }
